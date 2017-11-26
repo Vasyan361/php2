218dc2f66ea672b40265c05efc2e3604912ee9b6
Vasyan361 <vasyagromak@yandex.ru>
2017-11-26 23:08:14 +0300

    fouth commit

--- php2lesson1.local/article.php
+++ php2lesson1.local/article.php
@@ -1,35 +1,12 @@
-<!doctype html>
-
 <?php
 
-require __DIR__ . '/autoload.php';?>
+require __DIR__ . '/autoload.php';
 
-<html lang="ru">
+if (isset($_GET['id']) && false != \App\Models\Article::fyidById($_GET['id'])){
+    $article = \App\Models\Article::fyidById($_GET['id']);
+    include __DIR__ . '/App/Templates/article.php';
+} else{
+    http_response_code(404);
+};
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Статья</title>
-    </head>
 
-    <body>
-
-        <a href="/index.php">
-            <button>Назад</button>
-        </a>
-
-        <?php
-            if (isset($_GET['id']) && false != \App\Models\Article::fyidById($_GET['id'])){
-                $data = \App\Models\Article::fyidById($_GET['id']); ?>
-                <h1><?php echo $data->title; ?></h1>
-                <article><?php echo $data->lead; ?></article>
-            <?php  } else{
-                echo 'Такой новости не существует';
-            }
-        ?>
-
-    </body>
-
-</html>
--- php2lesson1.local/index.php
+++ php2lesson1.local/index.php
@@ -1,34 +1,6 @@
-<!doctype html>
-
 <?php
 
-require __DIR__ . '/autoload.php';?>
+require __DIR__ . '/autoload.php';
 
-<html lang="ru">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Новости</title>
-    </head>
-
-    <body>
-
-    <?php
-    foreach (\App\Models\Article::lastNews() as $article){?>
-
-
-        <a href="<?php echo '/article.php?id=' . $article->id; ?>">
-
-            <h1><?php echo $article->title; ?></h1>
-            <article><?php echo $article->lead; ?></article>
-
-        </a>
-
-    <?php } ?>
-
-    </body>
-
-</html>
+$news = \App\Models\Article::lastNews();
+include __DIR__ . '/App/Templates/index.php';
